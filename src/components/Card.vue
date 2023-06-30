2d6f2d9b68b31f053bdf30938a2e3b48ed41f5bc
MataVsn <113342160+MataVsn@users.noreply.github.com>
2023-06-30 11:41:11 +0300

    Card main logic.

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card card-style">
+        <div>
+            <h4>{{model.title}}</h4>
+            <p>{{model.desc}}</p>
+        </div>
+        <div>
+            <!-- V-if/else: Checking the status of a task and showing according button -->
+            <button v-if="!model.status">Add</button> 
+            <button v-else>Delete</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        model: { 
+            required: true, // Making our model neccessery
+            default: {
+                id: 0,
+                title: `Create`,
+                desc: `Create project`,
+                status: false // Prop for a button display
+            }
+        }
+    },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
