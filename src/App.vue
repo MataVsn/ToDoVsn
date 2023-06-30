f0cea3020f540a9c4af4b260d441782e94cae628
MataVsn <113342160+MataVsn@users.noreply.github.com>
2023-06-30 11:24:55 +0300

    Craeting components.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
+  <main>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
